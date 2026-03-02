bf1ba09807dde8338c37bcb86e944dc037382814
enedy <118657590+3n3dy@users.noreply.github.com>
2026-03-02 19:31:17 +0200

    fix: vite config + rename all JSX to .jsx

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   base: './',
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  resolve: {
+    alias: {
+      "@": path.resolve(".", "src")
+    }
+  },
+  build: { outDir: 'dist' }
 })
